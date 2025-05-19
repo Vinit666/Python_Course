a0179abd5c97c9c3f51f7def8953817e3228ae5f
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-19 18:24:22 +0530

    variable python files added

---
+++ variable.py
@@ -0,0 +1,17 @@
+print("this how yu define variables in python")
+print("Rules of variables defining : --->")
+print(" you can only use in first place of variable as ---> a-z / A-Z / _")
+print("use other 2nd to last place as ---> a-z / A-Z / _ / 0-9 ")
+print("you can't use special characters as in the variables. ")
+print("you cant ")
+
+_=1
+__=2
+___=3
+print(_+__+___)
+
+
+a_1 = 23
+b_2 = 2
+c_3 = 5
+print(a_1+b_2+c_3)
