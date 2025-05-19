c6e74b9e1c537b5a2a75edf4549c09a4629afdf2
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-19 17:59:21 +0530

    added comments python file

---
+++ comments.py
@@ -0,0 +1,22 @@
+# this is single line comments
+print("this is my single comments")
+print("select the lines and use \"ctrl+/\" for comment them.")
+
+# wbvhbvbjhvv
+# ev elvaev
+# wrvlnaelkv
+# rvanvnv
+# wwvvpnvav
+# wevnpvn
+
+print("to comment multiple by using multiple line comment use the symble \"\"\" / \''' before and after the lines. ")
+"""
+lnrbvievvrierrka evbeeerrvv
+erveb
+betebtbr
+TreeBuilder rt
+            bwrtbwrtbwrtrtbwb
+            writePlistToByteswtbw
+                              Textboxttbrbrrbwr
+
+"""
