b421f0365e00f40613e70fc5f236891632e2a6c4
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-20 14:38:19 +0530

    added logical operators file

---
+++ logical_operators.py
@@ -0,0 +1,20 @@
+a=40
+b=78
+c=20
+
+print(a>36)
+
+print(a>36 and b>36 and c>36) #and operator --true when all conditions are true else false
+
+a=20
+b=20
+print(a>b)
+print(a==b)
+print(a<b)
+print(a>=b)
+
+
+a=15
+b=20
+c=66
+print(a>36 or b>36 or c>36) #or operator --true when only one condition is true 
