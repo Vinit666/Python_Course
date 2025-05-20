94603bcf41bfa9ee63ae242a41b05a0150075cba
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-20 12:56:45 +0530

    added input variables file

---
+++ input_var.py
@@ -0,0 +1,7 @@
+a=input("Enter your name:--> ")
+num1=int(input("Enter the maths marks : "))
+num2=int(input("Enter the english marks : "))
+num3=int(input("Enter the hindi marks : "))
+
+total = num1+num2+num3
+print(f"Your name ---> {a}\n Your total marks ---> {total}")
