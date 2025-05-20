3c250014d6113c9e2090a00283c61f0b4c426975
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-20 12:49:10 +0530

    added type conversion file

---
+++ type_conversion.py
@@ -0,0 +1,21 @@
+# int to string converson
+a=45
+b=400
+c=50.45
+
+x=str(a)
+y=str(b)
+z=str(c)
+
+print(x+y+z)
+
+#string to int conversion
+
+d="34"
+e="45.66"
+f="500"
+
+p=int(d)
+q=float(e)
+r=int(f)
+print(p+q+r)
