4ec8c8b76ce29a48487c96419797c66275878ff2
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-20 12:14:54 +0530

    added data types file

---
+++ data_types.py
@@ -0,0 +1,11 @@
+#list
+print("this is a list example--->")
+a=[12,23,34,45.88,78.999,"vinit","kumawat"]
+print(a)
+
+
+
+#touple
+print("\nthis is a touple example--->")
+b=(1,34,56.66,34.66,"'vishal","kumawat",94.55,10000)
+print(b)
