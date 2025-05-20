62fd2ea5f7852944e1d47bd87d4f87d82060d01b
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-20 12:33:57 +0530

    updated data types

--- data_types.py
+++ data_types.py
@@ -1,16 +1,28 @@
+m=5
+print(m,type(m))
+
+n=20.999
+print(n,type(n))
+
+o="Code and Debug --> Python Cource"
+print(o,type(o))
+
+p=True
+print(p, type(p))
+
 #list
 print("this is a list example--->")
 a=[12,23,34,45.88,78.999,"vinit","kumawat"]
-print(a)
+print(a, type(a))
 
 
 
 #touple
 print("\nthis is a touple example--->")
 b=(1,34,56.66,34.66,"'vishal","kumawat",94.55,10000)
-print(b)
+print(b, type(b))
 
 #dictonary
 c={"vinit":21,"dinesh":22,"vishal":22}
 
-print(f"\nthis is dict example{c}")
+print(f"\nthis is dict example---> \n{c}",type(c))
