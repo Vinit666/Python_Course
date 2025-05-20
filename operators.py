7482ca9f7fffb83a514cebb9ab8fc721455f2cb1
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-20 14:01:28 +0530

    added operators file

---
+++ operators.py
@@ -0,0 +1,26 @@
+a=4
+a+=5
+print(a)
+
+a=5
+b=2
+print(a/b)
+
+a=20
+b=3
+print(a//b) #gives lowest integer value as a answer
+
+a=50
+b=20
+print(a%b) #gives the remender 
+
+a=8
+a=1
+print(a) #only mind the last define variables value in same varible multiple define
+
+a=2
+print(a**8) # used to define power to a value or a variable
+
+a=2
+b=5
+print(a**b)
