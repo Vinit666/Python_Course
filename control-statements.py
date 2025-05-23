5021ec6c6166633022cf79bb453cfb6b34cdd3a5
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-23 12:47:03 +0530

    Added control statements for while-loop and for-loop.

---
+++ control-statements.py
@@ -0,0 +1,95 @@
+"""
+control statements are three types ---> 1. break --->(breaks the loop in the middle by a using condition )
+                                        2. continue --->(using a condition excludes the line of code after the continue keyword and it sends the iterator to the starting of the loop)
+                                        3. pass --->(this do's nothing. in future , if you want to work for a condition then you can use pass in that condition and that code don't show the error. you can't blank a conditions that's why you use it.)
+"""
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+""" control ststements in the while loop
+"""
+
+# break statement 
+
+# i=1
+# while i<=20:
+#     if i==10:
+#         break
+#     print(i,end=" ")
+#     i+=1
+    
+    
+# continue statement
+
+# i=1
+# while i<=10:
+#     if i==5 or i==6:
+#         i+=1
+#         continue
+#     print(i,end=" ")
+#     i+=1
+
+
+# pass statement
+
+# i=1
+# while i<=10:
+#     if i==5:
+#         pass
+#     print(i, end=" ")
+#     i+=1
+    
+    
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+"""control ststements in the for loop
+"""
+
+# break statement 
+
+# for i in range(1,16):
+#     if i==10:
+#         break
+#     print(i,end=" ")
+    
+    
+# continue statement
+
+
+# for i in range (1,21):
+#     if i==5 or i==6 or i==10 or i==15:
+#         i+=1
+#         continue
+#     print(i,end=" ")
+
+
+
+# pass statement
+
+for i in range(1,16):
+    if i==5:
+        pass
+    print(i, end=" ")
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
