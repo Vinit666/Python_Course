20f4ba9ae7343ffbb97d3a79389fae7409249562
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-23 14:27:43 +0530

    Added pattern Q&A's in the file.

---
+++ nested-for-loop_Q&A.py
@@ -0,0 +1,148 @@
+"""
+Q.66 print the following pattern ?
+
+* * * * * 
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+"""
+
+# for i in range(1,6):
+#     for j in range(1,6):
+        
+#         print("*",end=" ")
+#     print("")
+
+"""
+Q.67 print the following pattern ?
+
+1 2 3 4 5 
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+
+"""
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(j,end=" ")
+#     print("")
+
+
+"""
+Q.68 print the following pattern ?
+
+5 4 3 2 1 
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+
+"""
+
+# for i in range (1,6):
+#     for j in range (5,0,-1):
+#         print(j,end=" ")
+#     print()
+
+"""
+Q.69 print the following pattern ?
+
+1 1 1 1 1 
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+
+"""
+# for i in range (1,6):
+#     for j in range(1,6):
+#         print(i,end=" ")
+#     print()
+
+"""
+Q.70 print the following pattern ?
+
+5 5 5 5 5 
+4 4 4 4 4
+3 3 3 3 3
+2 2 2 2 2
+1 1 1 1 1
+
+"""
+
+
+# for i in range(5,0,-1):
+#     for j in range(1,6):
+#         print(i,end=" ")
+#     print()
+
+"""
+Q.70 ask n from user and then print the following pattern ?
+
+Enter the value of N : 8
+1 1 1 1 
+2 2 2 2
+3 3 3 3
+4 4 4 4
+5 5 5 5
+6 6 6 6
+7 7 7 7
+8 8 8 8
+
+"""
+
+# n=int(input("Enter the value of N : "))
+# for i in range (1,n+1):
+#     for j in range(1,5):
+#         print(i,end=" ")
+#     print()
+
+"""
+Q.72 ask n from user and then print the following pattern ?
+
+Enter the value of N : 6
+6 6 6 6 6 
+5 5 5 5 5
+4 4 4 4 4
+3 3 3 3 3
+2 2 2 2 2
+1 1 1 1 1
+
+"""
+n=int(input("Enter the value of N : "))
+for i in range(n,0,-1):
+    for j in range(1,6):
+        print(i,end=" ")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
