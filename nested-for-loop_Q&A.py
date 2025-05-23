e41f51546743dcba192781a0dfa29966c205bd60
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-23 15:00:05 +0530

    Updated pattern Q&A's in the file.

--- nested-for-loop_Q&A.py
+++ nested-for-loop_Q&A.py
@@ -25,10 +25,10 @@ Q.67 print the following pattern ?
 1 2 3 4 5
 
 """
-for i in range(1,6):
-    for j in range(1,6):
-        print(j,end=" ")
-    print("")
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(j,end=" ")
+#     print("")
 
 
 """
