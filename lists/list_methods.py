6308a026dbf7a08b2013032758b45561dd83df23
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-25 15:51:05 +0530

    Added list-methods file.

---
+++ lists/list_methods.py
@@ -0,0 +1,83 @@
+
+""" Append()----> list_name.append(new _element)
+"""
+# a=[1,2,3,4,55,66,778,8,8,9,900,0,34,32,2,12,3,34,]
+# print(a)
+# a.append(12)
+# print(a)
+
+""" insert()----> list_name.append(index,new _element)
+---> if the index is not in range then it insert the element in the end of the list.
+"""
+# a=[1,2,3,4,55,66,778,8,8,9,900,0,34,32,2,12,3,34,1]
+# print(a)
+# a.insert(1,135)
+# print(a)
+
+# a=[1,2,3,4,55,66,778,8,8,9,900,0,34,32,2,12,3,34,]
+# print(a)
+# a.insert(300,13.5)
+# print(a)
+
+
+
+""" to update----> list_name[index] = new_element
+"""
+
+# a=[1,2,3,4,55,66,778,8]
+# print(a)
+# a[-2]=0
+# print(a)
+
+# a=[1,2,3,4,55,66,778,8]
+# print(a)
+# a[1]=90
+# print(a)
+
+""" to delete by position/index ----> list_name.pop(position)
+---> if there no position given to pop() then it will pop the last index value from the list.
+"""
+# a=[1,2,3,4,55,66,778,8,8,9,900,0,34,32,2,12,3,34,]
+# print(a)
+# a.pop(-1)
+# print(a)
+
+# a=[1,2,3,4,55,66,778,8,8,9,900,0,34,32,2,12,3,34,]
+# print(a)
+# a.pop()
+# print(a)
+
+
+
+""" to delete by value ----> list_name.remove(value)
+---> if there is more then 1 values in the list then it will remove the 1st same value that will come in the index.
+"""
+# a=[1,2,3,4,55,66,1,1,1,778,8,8,9,900,0,34,32,2,12,3,34]
+# print(a)
+# a.remove(1)
+# print(a)
+
+# a=[1,2,3,4,55,66,778,8,8,9,900,0,34,32,2,12,3,34]
+# print(a)
+# a.remove(8)
+# print(a)
+
+
+""" to delete----> del list_name[index]
+"""
+# a=[1,2,3,4,55,66,778,8,8,9,900,0,34,32,2,12,3,34]
+# print(a)
+# del a[1]
+# print(a)
+
+# a=[1,2,3,4,55,66,778,8,8,9,900,0,34,32,2,12,3,34,]
+# print(a)
+# del a
+# print(a)
+
+""" to empty a list----> list_name.clear()
+"""
+a=[1,2,3,4,55,66,778,8,8,9,900,0,34,32,2,12,3,34,]
+print(a)
+a.clear()
+print(a)
