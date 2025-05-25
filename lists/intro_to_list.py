c99e3a71eb22e83ec65646e250ed9c3e3c925962
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-25 12:42:06 +0530

    Added list intro file.

---
+++ lists/intro_to_list.py
@@ -0,0 +1,40 @@
+# list ---> we can store multiple elements in a same variable. and elemnets can be different data types.
+
+#list example
+
+# a=[12,34,2,33.4,45,33.9,"Vinit","Kumawat",False,True]
+# print(a)
+# print(type(a)) #checking type of variable that is list.
+
+# a=[12,34,2,33.4,45,33.9,"Vinit","Kumawat",False,True]
+# print(a)
+# print(type(a[0])) #checking list element data type.
+
+# print(len(a)) # checking lenght a list by len()
+
+
+
+
+# we can iterate list by index 2 tpyes.
+
+# 2. positive ndexing --- > iterates from left to right of list
+
+# a=[13,23,45,55,23,44.4]
+# print(a[0]) #by giving the index from 0,1,2,3,4,5.........n
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+# print(a[5])
+
+
+# 2. negative ndexing --- > iterates from right to left of list
+
+# a=[13,23,45,55,23,44.4]
+# print(a[-1]) #by giving the index from -1,-2,-3,-4,-5.........-n
+# print(a[-2])
+# print(a[-3])
+# print(a[-4])
+# print(a[-5])
+# print(a[-6])
+
