5da42b01a19d23156896ab32fd925dc342251bf9
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-27 13:33:19 +0530

    Added slicing of strings file.

---
+++ String/string_slicing.py
@@ -0,0 +1,24 @@
+# a="vinit kumawat"
+# print(a)
+# b=a[::-1]
+# print(b)
+
+# a="vinit kumawat"
+# print(a)
+# b=a[-5::]
+# print(b)
+
+# a="vinit kumawat"
+# print(a)
+# b=a[-4::-1]
+# print(b)
+
+# a="vinit kumawat"
+# print(a)
+# b=a[0:5:]
+# print(b)
+
+a="vinit kumawat"
+print(a)
+b=a[::]
+print(b)
