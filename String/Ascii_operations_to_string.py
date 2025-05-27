6438dcb220853fa458068efff484f49af81bef1d
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-27 13:22:05 +0530

    Added Ascii and arithmetic operations in strings file.

---
+++ String/Ascii_operations_to_string.py
@@ -0,0 +1,69 @@
+#arithmetic operations on strings
+ch1="Vinit"
+ch2="vinit"
+print(ch1 + ch2)
+
+# ch1="Vinit"
+# ch2="vinit"
+# print(ch1 * ch2) #we can't multiply the two strings
+
+ch1="Vinit"
+ch2="vinit"
+print(ch1 * 3) #we can multiply a single string to only any integer
+
+ch1="Vinit"
+ch2="vinit"
+print(ch2 * 5)
+
+# ASCII codes of strings and comparision
+
+ch1="Vinit"
+ch2="vinit"
+print(ch2 > ch1)
+
+ch1="viNit"
+ch2="viniT"
+print(ch1 > ch2)
+
+ch1="viNit"
+ch2="viniT"
+print(ch2 > ch1)
+
+ch1="a"
+ch2="A"
+print(ord(ch1))
+print(ord(ch2))
+print(ch1>ch2)
+
+ch1="a"
+ch2="A"
+ch3="9"
+print(ord(ch1)) # ord() only finds one charcter of string ascii code value
+print(ord(ch2))
+print(ord(ch3))
+print(ch1>ch2)
+
+ch1=90
+ch2=56
+print(chr(ch1)) # ord() only finds one charcter of string ascii code value
+print(chr(ch2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
