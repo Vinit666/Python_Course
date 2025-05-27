3a92f394a124b01c7479517f005a4a8b663c2891
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-27 11:49:49 +0530

    Added string-intro file.

---
+++ String/intro_to_string.py
@@ -0,0 +1,23 @@
+#string is immutuable
+#string is iteratable
+
+a="Hii, Vinit Kumawat"
+print(a)
+print(type(a))
+
+
+a="Hii, Vinit Kumawat"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+
+
+a="Hii, Vinit Kumawat"
+print(a[-1])
+print(a[-2])
+print(a[-3])
+print(a[-4])
+
+
