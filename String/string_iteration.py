75dbe66e974ea6d769db10fcf0a52713af18378f
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-05-27 11:54:59 +0530

    Added string iteration file.

---
+++ String/string_iteration.py
@@ -0,0 +1,18 @@
+#string is iteratable
+
+a="Vinit Kumawat"
+print(a)
+print(type(a))
+
+#iterate by index and iterate by value
+
+# #by index
+# a="Vinit Kumawat"
+# for index in range(0,len(a)):
+#     print(a[index])
+    
+    
+# by value
+a="Vinit Kumawat"
+for value in a:
+    print(value)
