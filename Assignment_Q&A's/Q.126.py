8665798edc871ffc675f6c040182d762569131c9
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-06-01 14:16:43 +0530

    Added Q126.py file

---
+++ Assignment_Q&A's/Q.126.py
@@ -0,0 +1,8 @@
+"""Q126. Write a python program to get every third element from a list using
+slicing.
+"""
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print(f"list is : {l}")
+l1 = l[::3]
+print(f"every 3rd element list is : {l1}")
