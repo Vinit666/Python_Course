faa8beb5a1adb00fd378b2d68613c575d9d47efa
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-06-01 14:11:40 +0530

    Added Q125.py file

---
+++ Assignment_Q&A's/Q.125.py
@@ -0,0 +1,6 @@
+"""125. Write a python program to reverse a list using slicing."""
+
+l = ["hello", 1, 2, 3, 4, 5, 6, "world"]
+print(f"list is : {l}")
+l2 = l[::-1]
+print(f"Reverse of list is : {l2}")
