87130d7210d57a10d935c17c54b65059c039ed8a
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-06-03 12:21:44 +0530

    Created day-2 and added args file

---
+++ Oops_with_functions/Day-2_args-kwargs_lambda'fn_annotations/args.py
@@ -0,0 +1,14 @@
+# def sum_of(*args):
+#     # print(args)  # args gives output in tuple.
+#     print(sum(args))
+
+
+# sum_of(1, 2, 3, 4, 4, 5)
+# sum_of()
+
+
+def sum_of(*args):
+    print(args)
+
+
+sum_of(23, [1, 2], [12], 23, 34)
