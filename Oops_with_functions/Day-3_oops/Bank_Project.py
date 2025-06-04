fa344e7ba25c23b6967b94a1024e7681f35b7dd2
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-06-04 13:46:54 +0530

    Modified Bank Project File

--- Oops_with_functions/Day-3_oops/Bank_Project.py
+++ Oops_with_functions/Day-3_oops/Bank_Project.py
@@ -50,7 +50,8 @@ while True:
     print("2.Show all bank details.")
     print("3.deposit amount.")
     print("4.Withdrawl amount.")
-    print("5.Exit.")
+    print("5.For money transction.")
+    print("6.Exit.")
 
     choice = int(input("Enter the choice : "))
 
@@ -59,27 +60,71 @@ while True:
         banks.append(obj)
 
     elif choice == 2:
-        print(banks)
-        for obj in banks:
-            obj.info()
+        if len(banks) == 0:
+            print("There is no account found.\nFirst, create a account.")
+        else:
+            for obj in banks:
+                obj.info()
 
     elif choice == 3:
-        acc_num = int(input("For Deposit, Enter the Account no. : "))
-        found = 0
-        for obj in banks:
-            if acc_num == obj.acc_no:
-                obj.deposit()
-                found += 1
-        if found == 0:
-            print("Account does not exist.")
+        if len(banks) == 0:
+            print("There is no account found.\nFirst, create a account.")
+        else:
+            acc_num = int(input("For Deposit, Enter the Account no. : "))
+            found = 0
+            for obj in banks:
+                if acc_num == obj.acc_no:
+                    obj.deposit()
+                    found += 1
+            if found == 0:
+                print("Account does not exist.")
 
     elif choice == 4:
-        acc_num = int(input("For Withdrawl, Enter your Account no. : "))
-        for obj in banks:
-            if acc_num == obj.acc_no:
-                obj.withdrawl()
-
+        if len(banks) == 0:
+            print("There is no account found.\nFirst, create a account.")
+        else:
+            acc_num = int(input("For Withdrawl, Enter your Account no. : "))
+            found = 0
+            for obj in banks:
+                if acc_num == obj.acc_no:
+                    obj.withdrawl()
+                    found += 1
+            if found == 0:
+                print("Account does not exist.")
     elif choice == 5:
+        if len(banks) == 0:
+            print("There is no account found.\nFirst, create a account.")
+        else:
+            Sender_acc_num = int(
+                input("For Transction, Enter Sender's(Your) Account no. : ")
+            )
+            Sender_found = 0
+            for Sender_obj in banks:
+                if Sender_acc_num == Sender_obj.acc_no:
+                    Receiver_acc_num = int(
+                        input("For Transction, Enter Receiver's Account no. : ")
+                    )
+                    Receiver_found = 0
+                    for Receiver_obj in banks:
+                        if Receiver_acc_num == Receiver_obj.acc_no:
+                            Receiver_found += 1
+                            transfer_amount = int(
+                                input("For Transfer, Enter the amount : ")
+                            )
+                            if transfer_amount <= Sender_obj.current_bal:
+                                Sender_obj.current_bal -= transfer_amount
+                                Receiver_obj.current_bal += transfer_amount
+                            else:
+                                print("Insuficient bank balance.")
+
+                    if Receiver_found == 0:
+                        print("Receiver's Account does not exist.")
+                    Sender_found += 1
+
+            if Sender_found == 0:
+                print("Sender's Account does not exist.")
+
+    elif choice == 6:
         break
 
 # l = [1, 2, 3, 4, 5, 6, 7]
