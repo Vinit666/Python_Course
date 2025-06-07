54218e340a7c138f9ee2a8688a3a51e5b89856a3
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-06-07 11:47:00 +0530

    Added Threading concept file

---
+++ Oops_with_functions/Day-10/Threads.py
@@ -0,0 +1,25 @@
+# threads--->completing a process in parts concurrently for fast processing or execution.
+
+import threading
+
+
+def print_using_for(num):
+    for i in range(1, num + 1):
+        print(i)
+
+
+def print_using_while(num):
+    i = 1
+    while i <= num:
+        print(i)
+        i += 1
+
+
+t1 = threading.Thread(target=print_using_for, args=(10,))
+t2 = threading.Thread(target=print_using_while, args=(10,))
+
+t1.start()
+t2.start()
+
+# t1.join()
+# t2.join()
